310c148bd6a341caedc77c68a6df0394baffad17
Akhtar Husain <akhtar4660@gmail.com>
2018-02-12 00:02:13 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,56 @@
+(function($) {
+    $.fn.countTo = function(options) {
+        options = $.extend({}, $.fn.countTo.defaults, options || {});
+
+        var loops = Math.ceil(options.speed / options.refreshInterval),
+            increment = (options.from - options.to) / loops;
+		Number.prototype.formatMoney = function(c, d, t){
+		var n = this, 
+			c = isNaN(c = Math.abs(c)) ? 2 : c, 
+			d = d == undefined ? "." : d, 
+			t = t == undefined ? "," : t, 
+			s = n < 0 ? "-" : "", 
+			i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+			j = (j = i.length) > 3 ? j % 3 : 0;
+		   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+		 };
+
+        return $(this).each(function() {
+            var _this = this,
+                loopCount = 0,
+                value = options.from,
+                interval = setInterval(updateTimer, options.refreshInterval);
+
+            function updateTimer() {
+                value -= increment;
+                loopCount++;
+				var newVal = value.formatMoney(options.decimals, options.separator) + '%';
+                $(_this).html(newVal);
+
+                if (typeof(options.onUpdate) == 'function') {
+                    options.onUpdate.call(_this, value);
+                }
+
+                if (loopCount >= loops) {
+                    clearInterval(interval);
+                    value = options.to;
+
+                    if (typeof(options.onComplete) == 'function') {
+                        options.onComplete.call(_this, value);
+                    }
+                }
+            }
+        });
+    };
+
+    $.fn.countTo.defaults = {
+        from: 100,
+        to: 10,
+        speed: 500,
+        refreshInterval: 100,
+        decimals: 0,
+        onUpdate: null,
+        onComplete: null,
+		separator: '.'
+    };
+})(jQuery);
